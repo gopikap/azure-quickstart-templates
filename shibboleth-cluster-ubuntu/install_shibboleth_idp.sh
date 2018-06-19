4f15816f7ff6b56a48717ded8a75fe1e945a0a3a
LakshmanKatiki <mca.lakshman@gmail.com>
2018-06-19 13:11:59 +0530

    Install Shibboleth
    Install Shibboleth

--- shibboleth-cluster-ubuntu/install_shibboleth_idp.sh
+++ shibboleth-cluster-ubuntu/install_shibboleth_idp.sh
@@ -10,14 +10,24 @@ DBSITENAME=$3.$2.cloudapp.azure.com
 INSTALLDIR=/opt/shibboleth-idp
 
 apt-get -y update
-apt-get -y upgrade
-apt-get -y install unzip
 
+echo "==============>Printing values of all variables"
+echo "domain"
+echo $domain
+echo "location"
+echo $location
+echo "Sitename"
+echo $SITENAME
+
+
+#install Oracle JDK 7
 echo debconf shared/accepted-oracle-license-v1-1 select true | \
 sudo debconf-set-selections
   
 echo debconf shared/accepted-oracle-license-v1-1 seen true | \
 sudo debconf-set-selections
+
+echo "==============>Installing JDK 7"
 
 apt-get -y install python-software-properties
 add-apt-repository -y ppa:webupd8team/java
@@ -25,37 +35,46 @@ apt-get -y update
 apt-get -y install oracle-java7-installer
 
 
+#install Tomcat 7
+
+echo "==============>Installing Tomcat7"
+
 apt-get -y install tomcat7
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+export PATH=$PATH:$JAVA_HOME/bin
+export JRE_HOME=/usr/lib/jvm/java-7-oracle/jre
+export JAVA_OPTS="-XX:+AggressiveOpts -Xms256m -Xmx512m -XX:MaxPermSize=256m -XX:+DisableExplicitGC"
+
+sed -i 's/#AUTHBIND=no/AUTHBIND=yes/g' /etc/default/tomcat7
+
+#generating the self signed SSL certificate for tomcat7
+
+echo "==============>Configuring SSL for Tomcat7"
+
+mkdir /usr/share/tomcat7/keystore
+cd $JAVA_HOME/bin
 
 SSLKEYPASSWORD=$(openssl rand -base64 12)
 
-mkdir /usr/share/tomcat7/keystore
-keytool -genkey -alias tomcat -keyalg RSA -keystore /usr/share/tomcat7/keystore/server.keystore -keysize 2048 -storepass $SSLKEYPASSWORD -keypass $SSLKEYPASSWORD -dname "cn=testname, ou=shibbolethOU, o=shibbolethO, c=US"
-sed -i '/redirectPort="8443"/a  <Connector port="8443"  protocol="org.apache.coyote.http11.Http11NioProtocol" SSLEnabled="true" maxThreads="150" scheme="https" secure="true"  clientAuth="false" sslProtocol="TLS" address="0.0.0.0" keystoreFile="/\usr/\share\/tomcat7/\keystore/\server.keystore" keystorePass="'$SSLKEYPASSWORD'"/>' /var/lib/tomcat7/conf/server.xml
-
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-echo JAVA_HOME='"'$JAVA_HOME'"' >> /etc/environment
-source /etc/environment
-
-echo export JAVA_HOME=/usr/lib/jvm/java-7-oracle >> /etc/profile
-echo export CATALINA_HOME=/var/lib/tomcat7 >> /etc/profile
-source /etc/profile
+keytool -genkey -alias tomcat -keyalg RSA -keystore /usr/share/tomcat7/keystore/server.keystore -keysize 2048 -storepass $SSLKEYPASSWORD -keypass $SSLKEYPASSWORD -dname "cn=$SITENAME, ou=shibbolethOU, o=shibbolethO, c=US"
+sed -i -e 's,redirectPort="8443",redirectPort="8443" address="0.0.0.0",g' /var/lib/tomcat7/conf/server.xml
+sed -i '/redirectPort="8443"/a  <Connector port="8443" protocol="HTTP/1.1" SSLEnabled="true" maxThreads="150" scheme="https" secure="true"  clientAuth="false" sslProtocol="TLS" address="0.0.0.0" keystoreFile="/\usr/\share\/tomcat7/\keystore/\server.keystore" keystorePass="'$SSLKEYPASSWORD'"/>' /var/lib/tomcat7/conf/server.xml
 
 
-sed -i 's,</tomcat-users>,  <role rolename="manager-gui"/>\n  <user username="admin" password="secret" roles="manager-gui"/>  \n</tomcat-users>,g'   /var/lib/tomcat7/conf/tomcat-users.xml
+#download jstl as it is not distributed with tomcat7 and is required by Shibboleth
 
-#Change 128m to 512m
-sed -i 's/128m/512m/g'  /etc/default/tomcat7
+echo "==============>Adding JSTL to Tomcat7"
 
-touch /etc/authbind/byport/8443
-chmod 0755 /etc/authbind/byport/8443
-chown tomcat7:tomcat7 /etc/authbind/byport/8443
+cd /usr/share/tomcat7/lib
+wget http://central.maven.org/maven2/jstl/jstl/1.2/jstl-1.2.jar
+chmod 777 jstl-1.2.jar
 
+#START Shibboleth installation
 
-SCOPE=$(hostname -f)
-
+echo "==============> Start Shibboleth installation"
+echo "==============> Download Shibboleth"
+apt-get -y install unzip
 cd /usr/share
-
 # install Shibboleth
 wget http://shibboleth.net/downloads/identity-provider/3.3.3/shibboleth-identity-provider-3.3.3.zip -O shibboleth.zip
 unzip shibboleth.zip
@@ -63,8 +82,13 @@ unzip shibboleth.zip
 cd shibboleth-identity-provider-3.3.3
 chmod -R +x bin
 
+# generate a password for client-side encryption
 echo "idp.sealer.password = $(openssl rand -base64 12)" >credentials.properties
 chmod 0600 credentials.properties
+
+# preconfigure settings for a typical deployment
+
+echo "==============> Generate preconfig file"
 
 cat >temp.properties <<EOF
 idp.additionalProperties= /conf/ldap.properties, /conf/saml-nameid.properties, /conf/services.properties, /conf/credentials.properties
@@ -74,12 +98,12 @@ idp.signing.key= %{idp.home}/credentials/idp.key
 idp.signing.cert= %{idp.home}/credentials/idp.crt
 idp.encryption.key= %{idp.home}/credentials/idp.key
 idp.encryption.cert= %{idp.home}/credentials/idp.crt
-idp.entityID= https://$SITENAME:8443/idp/shibboleth
-idp.scope= $SCOPE
+idp.entityID= https://$SITENAME/idp/shibboleth
+idp.scope= $SITENAME
 idp.consent.StorageService= shibboleth.JPAStorageService
 idp.consent.userStorageKey= shibboleth.consent.AttributeConsentStorageKey
 idp.consent.userStorageKeyAttribute= %{idp.persistentId.sourceAttribute}
-idp.consent.allowGlobal= true
+idp.consent.allowGlobal= false
 idp.consent.compareValues= true
 idp.consent.maxStoredRecords= -1
 idp.ui.fallbackLanguages= en,de,fr
@@ -102,35 +126,34 @@ bash bin/install.sh \
 -Didp.scope=$SITENAME
 
 chown -R tomcat7 /opt/shibboleth-idp
-
 cd /opt/shibboleth-idp/edit-webapp/WEB-INF/lib
 
-echo "==============>Adding JSTL to tomcat7"
-
-wget https://build.shibboleth.net/nexus/service/local/repositories/thirdparty/content/javax/servlet/jstl/1.2/jstl-1.2.jar
-chmod 777 jstl-1.2.jar
-chown tomcat7 jstl-1.2.jar
+#edit all location's port 8443 /opt/shibboleth-idp/metadata/idp.metadata
+echo "==============> Updating the urls in metadata.xml"
 
 sed -i -e 's,https://'"$SITENAME"'/idp/profile/Shibboleth/SSO,https://'"$SITENAME"':8443/idp/profile/Shibboleth/SSO,g' /opt/shibboleth-idp/metadata/idp-metadata.xml
 sed -i -e 's,https://'"$SITENAME"'/idp/profile/SAML2/POST/SSO,https://'"$SITENAME"':8443/idp/profile/SAML2/POST/SSO,g' /opt/shibboleth-idp/metadata/idp-metadata.xml
 sed -i -e 's,https://'"$SITENAME"'/idp/profile/SAML2/POST-SimpleSign/SSO,https://'"$SITENAME"':8443/idp/profile/SAML2/POST-SimpleSign/SSO,g' /opt/shibboleth-idp/metadata/idp-metadata.xml
 sed -i -e 's,https://'"$SITENAME"'/idp/profile/SAML2/Redirect/SSO,https://'"$SITENAME"':8443/idp/profile/SAML2/Redirect/SSO,g' /opt/shibboleth-idp/metadata/idp-metadata.xml
 
-echo "<Context docBase=\"/opt/shibboleth-idp/war/idp.war\" privileged=\"true\" antiResourceLocking=\"false\" antijarLocking=\"false\" unpackWar=\"false\" swallowOutput=\"true\" />" > /var/lib/tomcat7/conf/Catalina/localhost/idp.xml
+# Use context deployment fragment for deploying idp.war file
+echo "==============> Adding application to tomcat7"
 
+echo "<Context docBase=\"/opt/shibboleth-idp/war/idp.war\" privileged=\"true\" antiresourcelocking=\"false\" antijarlocking=\"false\" unpackwar=\"false\" swallowoutput=\"true\" />" > /var/lib/tomcat7/conf/Catalina/localhost/idp.xml
 mv  /usr/share/shibboleth-identity-provider-3.3.3/credentials.properties $INSTALLDIR/conf
 
 cd /opt/shibboleth-idp
+
 echo -e "\nCreating self-signed certificate..."
 bin/keygen.sh --lifetime 3 \
 --certfile $INSTALLDIR/credentials/idp.crt \
 --keyfile $INSTALLDIR/credentials/idp.key \
 --hostname $SITENAME \
---uriAltName https://$SITENAME:8443/idp/shibboleth
+--uriAltName https://$SITENAME/idp/shibboleth
 echo ...done
 chmod 600 $INSTALLDIR/credentials/idp.key
 
-
+# set owner of key file and directories
 getent passwd tomcat7 >/dev/null && TCUSER=tomcat7 || TCUSER=tomcat
 chown $TCUSER $INSTALLDIR/credentials/idp.key
 chown $TCUSER $INSTALLDIR/credentials/sealer.*
@@ -149,5 +172,9 @@ sed -i "s/#idp.consent.StorageService.*/idp.consent.StorageService = shibboleth.
 sed -i "s/#idp.replayCache.StorageService.*/idp.replayCache.StorageService = shibboleth.JPAStorageService/" /opt/shibboleth-idp/conf/idp.properties
 sed -i "s/#idp.artifact.StorageService.*/idp.artifact.StorageService = shibboleth.JPAStorageService/" /opt/shibboleth-idp/conf/idp.properties
 
-bin/build.sh -Didp.target.dir=/opt/shibboleth-idp
+#add mysql-connector-java.jar in /tomcat7/lib
+cd /usr/share/tomcat7/lib
+wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.6/mysql-connector-java-5.1.6.jar
+
+#restart tomcat
 service tomcat7 restart
